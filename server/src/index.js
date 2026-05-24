9604abb4c3fa294f46754696546535449e21580c
Samagra78 <185402698+Samagra78@users.noreply.github.com>
2026-05-24 13:29:28 +0000

    add: add all route imports

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
-const organizationManagementRoutes = require('./routes/api/orgManagement.js');
-const boardManagementRoutes = require('./routes/api/boardManagement.js');
-const issueTrackingRoutes = require('./routes/api/issueTracking.js');
-const sprintManagementRoutes = require('./routes/api/sprintManagement.js');
+const organizationRoutes = require('./routes/api/orgRoutes.js');
+const boardRoutes = require('./routes/api/boardRoutes.js');
+const issueRoutes = require('./routes/api/issueRoutes.js');
+const sprintRoutes = require('./routes/api/sprintRoutes.js');
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/organizations", organizationManagementRoutes);
-app.use("/api/boards", boardManagementRoutes);
-app.use("/api/issues", issueTrackingRoutes);
-app.use("/api/sprints", sprintManagementRoutes);
+app.use("/api/organizations", organizationRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/issues", issueRoutes);
+app.use("/api/sprints", sprintRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
